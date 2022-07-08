46bd464f1f77fb78b3517589273adefb8f5d94c1
Muhammad Taufan <mhmmd.taufan@gmail.com>
2022-07-08 23:37:39 +0700

    Add Contructor and Method

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        var player1 = new Player();
-        player1.name = "Komo";
+        var player1 = new Player("Komo");
         player1.power = 84;
         player1.speed = 37;
         player1.reach = 20;
+        player1.star = 3;
+        player1.setMoney(50);
 
-        var player2 = new Player();
-        player2.name = "Snowbi";
+        var player2 = new Player("Snowbi");
         player2.power = 49;
         player2.speed = 90;
         player2.reach = 50;
+        player2.star = 2;
+        player2.setMoney(35);
 
-        System.out.println("Name  : " + player1.name + " vs " + player2.name);
-        System.out.println("Power : " + player1.power + " || " + player2.power);
-        System.out.println("Speed : " + player1.speed + " || " + player2.speed);
-        System.out.println("Reach : " + player1.reach + " || " + player2.reach);
+        System.out.println("Player Profile : ");
+        System.out.println("-------------------------------");
+
+        System.out.println("Name    : " + player1.name);
+        System.out.println("Power   : " + player1.power);
+        System.out.println("Speed   : " + player1.speed);
+        System.out.println("Reach   : " + player1.reach);
+        System.out.println("Star    : " + player1.star);
+        System.out.println("Money   : " + player1.money);
+        System.out.println("Health  : " + player1.health);
+
+        System.out.println("-------------------------------");
+
+        System.out.println("Name    : " + player2.name);
+        System.out.println("Power   : " + player2.power);
+        System.out.println("Speed   : " + player2.speed);
+        System.out.println("Reach   : " + player2.reach);
+        System.out.println("Star    : " + player2.star);
+        System.out.println("Money   : " + player2.money);
+        System.out.println("Health  : " + player2.health);
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,38 @@
+import java.time.LocalDateTime;
+
 class Player {
     String name;
-    Integer power;
-    Integer speed;
-    Integer reach;
+    int power;
+    int speed;
+    int reach;
+    int health;
+    int money;
+    int star;
+
+    Player(String playerName) {
+        name = playerName;
+        health = setHealthBasedOnTime();
+    }
+
+    void setMoney(int playerMoney) {
+        money = star * playerMoney;
+    }
+
+    int setHealthBasedOnTime() {
+        int tempHealth = 0;
+        LocalDateTime currentTime = LocalDateTime.now();
+
+        int currentHour = currentTime.getHour();
+        if (currentHour >= 0 && currentHour < 8) {
+            tempHealth = 80;
+        }
+        else if (currentHour >= 8 && currentHour < 16) {
+            tempHealth = 100;
+        }
+        else if (currentHour >= 16 && currentHour <= 23) {
+            tempHealth = 70;
+        }
+
+        return tempHealth;
+    }
 }
